cbeb8f4a3b5793f0d30512fc972f06aa69eeccba
cflin <cflin@mail.cjcu.edu.tw>
2020-04-24 10:43:57 +0800

    刪除確定按鈕的功能

--- app/src/main/java/com/example/test0318/MainActivity.java
+++ app/src/main/java/com/example/test0318/MainActivity.java
@@ -33,30 +33,30 @@ public class MainActivity extends AppCompatActivity {
         weight = (EditText) findViewById(R.id.ed_weight);
     }
 
-    public void show(View view) {
-
-        TextView b =  (TextView) findViewById(R.id.tv_showbmi);
-        ImageView view1 = (ImageView) findViewById(R.id.imageView);
-        String h = height.getText().toString();
-        String w = weight.getText().toString();
-        double bmi = Double.parseDouble(w)  / (Double.parseDouble(h) /100.0 * Double.parseDouble(h)/100.0);
-        DecimalFormat df = new DecimalFormat("#.#");
-
-
-        String msg="";
-        if(bmi<18.5) {
-            msg ="過輕";
-            view1.setImageResource(R.drawable.a1);
-        } else if (bmi>25) {
-            msg = "過重";
-            view1.setImageResource(R.drawable.a3);
-        } else {
-            msg = "標準";
-            view1.setImageResource(R.drawable.a2);
-        }
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
-        b.setText(name.getText().toString()+"您的'BMI是" + df.format(bmi));
-    }
+//    public void show(View view) {
+//
+//        TextView b =  (TextView) findViewById(R.id.tv_showbmi);
+//        ImageView view1 = (ImageView) findViewById(R.id.imageView);
+//        String h = height.getText().toString();
+//        String w = weight.getText().toString();
+//        double bmi = Double.parseDouble(w)  / (Double.parseDouble(h) /100.0 * Double.parseDouble(h)/100.0);
+//        DecimalFormat df = new DecimalFormat("#.#");
+//
+//
+//        String msg="";
+//        if(bmi<18.5) {
+//            msg ="過輕";
+//            view1.setImageResource(R.drawable.a1);
+//        } else if (bmi>25) {
+//            msg = "過重";
+//            view1.setImageResource(R.drawable.a3);
+//        } else {
+//            msg = "標準";
+//            view1.setImageResource(R.drawable.a2);
+//        }
+//        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+//        b.setText(name.getText().toString()+"您的'BMI是" + df.format(bmi));
+//    }
 
     public void nextPage(View view) {
         Bundle bundle = new Bundle();
